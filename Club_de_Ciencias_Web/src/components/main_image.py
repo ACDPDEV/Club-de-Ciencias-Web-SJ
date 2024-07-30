ad4d3b9a0c6e238aaab59a09585f2af06cafda45
ACDPDEV <acdpdev@gmail.com>
2024-07-29 21:13:38 -0500

    modified:   Club_de_Ciencias_Web/src/components/main_image.py 	new file:   assets/ASA.svg 	new file:   assets/Background Icons.svg 	new file:   assets/Club de Ciencias.png 	deleted:    assets/main_image.jpg

--- Club_de_Ciencias_Web/src/components/main_image.py
+++ Club_de_Ciencias_Web/src/components/main_image.py
@@ -3,12 +3,6 @@ import reflex as rx
 
 def main_image() -> rx.Component:
     return rx.vstack(
-        rx.image(
-            src="/main_image.jpg",
-            min_width="100%",
-            min_height="100%",
-            filter="blur(10px)"
-        ),
         width="100%",
         height="90vh",
         bg="#574710"
